ff8c08dfabdd1c13963a7ae11d5ed2c9891c40a9
David5g12 <145526876+David5g12@users.noreply.github.com>
2023-10-29 21:34:44 -0600

    Agregar archivos de proyecto.

---
+++ practica 12 C Sharp/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+namespace TABLAS_DE_MULTIPLICAR_CON_WHILE
+{
+    class program
+    {
+        static void Main(string[] args)
+        {
+            int NUM, RESUL, T, I;
+            string linea;
+            Console.Write("CUANTAS TABLAS: ");
+            linea = Console.ReadLine();
+            NUM = int.Parse(linea);
+            T = 1;
+            while ((T <= NUM))
+            {
+                I = 10;
+                while (!((I < 1)))
+                {
+                    RESUL = T * I;
+                    Console.WriteLine("{0} * {1} = {2}", T, I, RESUL);
+                    I = I - 1;
+                }
+                Console.Write("pulse una tecla:");
+                Console.ReadLine();
+                T = T + 1;
+            }
+        }
+    }
+}
